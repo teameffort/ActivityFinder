b14c14e64878a82a9e47fe31778417f44a248a44
teameffort <teameffort6@gmail.com>
2014-03-24 15:43:34 +0200

    Initial update

---
+++ DAL/TE.ActivityFinder.DAL/Context.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Configuration;
+
+namespace TE.ActivityFinder.DAL
+{
+    public class Context : IDisposable
+    {
+
+        #region Properties
+
+        private static string _connectionString;
+        public static string ConnectionString
+        {
+            get
+            {
+                if (_connectionString == null) 
+                {
+                    _connectionString = ConfigurationManager.ConnectionStrings["TE.ActivityFinder.POC.DAL.Properties.Settings.TE_ActivityFinder_POC_DBConnectionString"].ConnectionString;
+                }
+                return _connectionString; 
+            }
+        }
+
+        private PocDataContext _poc;
+        public PocDataContext Poc
+        {
+            get
+            {
+                if (_poc == null)
+                {
+                    _poc = new PocDataContext(ConnectionString);
+                }
+                return _poc;
+            }
+            set
+            {
+                _poc = value;
+            }
+        }
+
+        #endregion
+
+        #region IDisposable Implementation
+
+        public void Dispose()
+        {
+            if (_poc != null)
+            {
+                _poc.Dispose();
+            }
+        }
+
+        #endregion
+
+    }
+}
