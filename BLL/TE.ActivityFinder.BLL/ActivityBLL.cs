0636be02ab56d3e64ef78b4869d2251a737fa2e5
teameffort <teameffort6@gmail.com>
2014-03-25 14:46:37 +0200

    Added missing files

---
+++ BLL/TE.ActivityFinder.BLL/ActivityBLL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TE.ActivityFinder.DAL;
+
+namespace TE.ActivityFinder.BLL
+{
+    public class ActivityBLL
+    {
+        public List<act_GetActivitiesResult> act_GetActivities()
+        {
+            using (var context = new Context().Activity)
+            {
+                return context.act_GetActivities().ToList();               
+            }
+        }
+
+        public bool loc_InsertLocation(string Name, string Longitude, string Latitude)
+        {
+            using (var context = new Context().Activity)
+            {
+                int result;
+                result = context.loc_InsLocation(Name, Longitude, Latitude);
+                return result == 0 ? true : false;
+            }
+        }
+    }
+}
---
+++ WCF/TE.ActivityFinder.WCF/IActivityService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using TE.ActivityFinder.BLL;
+using TE.ActivityFinder.DAL;
+
+namespace TE.ActivityFinder.WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IActivityService
+    {
+
+        [OperationContract]
+        List<act_GetActivitiesResult> act_GetActivities();
+
+    }
+}
