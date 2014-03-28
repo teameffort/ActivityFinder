e59e65f740e0bfb9a2c7783daef7ff274ccee6b7
teameffort <teameffort6@gmail.com>
2014-03-28 17:00:15 +0200

    refactored js files

---
+++ UI/TE.ActivityFinder.UI.HTML5/Scripts/te.activityfinder.wcf.activityservice.js
@@ -0,0 +1,9 @@
+﻿function GetActivities() {
+    Type = "GET";
+    Url = "http://10.211.55.3/TE.ActivityFinder.WCF/ActivityService.svc/act_GetActivities";
+    ContentType = "application/json; charset=utf-8";
+    DataType = "json";
+    ProcessData = false;
+    method = "act_GetActivities";
+    CallService();
+}
